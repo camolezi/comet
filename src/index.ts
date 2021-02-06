e854e2f12b827b58742e2498a8532fa109807926
camolezi <camolezi@usp.br>
2021-02-06 12:03:38 -0300

    Updated dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,33 @@
 import express from "express";
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use((req, res, next) => {
+  console.log("middleware1");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("middleware2");
+  next();
+});
+
+app.get("/", (req, res, next) => {
+  console.log("response1");
+  next();
+});
+
+app.get("/", (req, res, next) => {
+  console.log("response2");
+
+  if (false) res.send("Hello World2");
+  else next();
+});
+
+app.use("/", (req, res) => {
+  console.log("Error");
+  res.send("Error");
 });
 
 app.listen(port, () => {
