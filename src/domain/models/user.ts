db0bdb5310129db2829fb1f2b7fcd3d81f427765
camolezi <camolezi@usp.br>
2021-02-08 21:46:17 -0300

    Created mongoose user schema

---
+++ src/domain/models/user.ts
@@ -0,0 +1,13 @@
+export interface User {
+  name: string;
+  email: string;
+}
+
+import { Schema } from "mongoose";
+
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+});
+
+export default UserSchema;
