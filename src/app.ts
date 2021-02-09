8c093286c662f268b2d26a8d65c9ad7d9d0100c7
camolezi <camolezi@usp.br>
2021-02-08 21:50:35 -0300

    Implemented the functionality for creating a new user
    The controller is calling the UserRepository, but in the future we should create a service class, and let the controller only handling routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import UserController from "./controllers/user_controller";
+import { startDBConnection } from "./database/db.connection";
+import UserRepository from "./database/user.repository";
 
 const app = express();
 
@@ -9,6 +11,9 @@ app.get("/version", (req, res) => {
   res.json({ version: "1.0" });
 });
 
-app.use("/users", UserController);
+const userConn = startDBConnection();
+const userRepository = new UserRepository(userConn);
+
+app.use("/users", UserController(userRepository));
 
 export default app;
--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -1,10 +1,18 @@
-import express from "express";
+import { Router } from "express";
+import { StatusCodes } from "http-status-codes";
+import UserRepository from "../database/user.repository";
 
-const UserController = express.Router();
+function UserController(userRepository: UserRepository): Router {
+  const userRouter = Router();
 
-UserController.post("/", (req, res) => {
-  res.status(201);
-  res.json(req.body);
-});
+  userRouter.post("/", async (req, res) => {
+    res.status(StatusCodes.CREATED);
+
+    const createdUser = await userRepository.saveNewUser(req.body);
+    res.json(createdUser.toJSON());
+  });
+
+  return userRouter;
+}
 
 export default UserController;
