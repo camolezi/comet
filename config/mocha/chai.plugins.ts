b74ebd66ef9db8e34cde1235d3e19497126e2baa
camolezi <camolezi@usp.br>
2021-02-08 21:44:58 -0300

    Installed mongo driver and mongoose
    Also installed chaiExclude. Renamed mocha hooks files

--- config/mocha/chai.plugins.ts
+++ config/mocha/chai.plugins.ts
@@ -1,9 +1,11 @@
 import chai from "chai";
 import chaiHttp from "chai-http";
+import chaiExclude from "chai-exclude";
 
 export const mochaHooks = {
   beforeAll(done) {
     chai.use(chaiHttp);
+    chai.use(chaiExclude);
     done();
   },
 };
